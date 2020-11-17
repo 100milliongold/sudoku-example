d3623a0a2e9682bf9ee91eb80c7e7a63679a217e
je.kim <gadian88@gmail.com>
2020-11-18 01:05:45 +0900

    15. Styling Block Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { ThemeProvider } from 'styled-components';
-
-import { GlobalStyles, theme } from './styles';
-import { unregister, reportWebVitals } from './core';
+import { ThemeProvider } from 'styled-components'
+import { Card, Content, Grid, Title } from './components'
+import { GlobalStyles, theme } from './styles'
+import { unregister, reportWebVitals } from './core'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <GlobalStyles/>
-    <div>
-      Hello world
-    </div>
+    <GlobalStyles />
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-unregister();
+unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,27 +1,26 @@
 import { createGlobalStyle, css } from 'styled-components'
 
-
 export default createGlobalStyle`
-    ${ ( { theme } ) => css`
-        html{
-            height: 100%;
+  ${({ theme }) => css`
+    html {
+      height: 100%;
 
-            body{
-                display: flex;
-                flex-direction: column;
-                height: 100%;
-                margin: 0;
+      body {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        margin: 0;
 
-                #root{
-                    background: ${theme.colors.background};
-                    color: ${theme.colors.black};
-                    display: flex;
-                    font-family: sans-serif;
-                    height: 100%;
-                    padding: 15px;
-                    justify-content: center;
-                }
-            }
+        #root {
+          background: ${theme.colors.background};
+          color: ${theme.colors.black};
+          display: flex;
+          font-family: sans-serif;
+          height: 100%;
+          justify-content: center;
+          padding: 15px;
         }
-    `}
-`;
+      }
+    }
+  `}
+`
