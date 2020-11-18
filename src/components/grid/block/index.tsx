f3bf17b93fc490e09546711d3b32822a7fcb5cdd
xiilab <je.kim@xiilab.com>
2020-11-18 13:21:41 +0900

    17. Absolute Imports and Types

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -7,7 +7,7 @@ interface IProps {
 }
 
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`}></Container>
+  return <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container>
 }
 
 export default Block
