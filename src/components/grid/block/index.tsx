c6b44917545432baf569aa6a6ddc343f2ab2ed0c
100milliongold <gadian88@gmail.com>
2020-12-01 00:10:21 +0900

    33. Selecting Blocks

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,13 +1,29 @@
 import React, { FC } from 'react'
+import { useSelector } from 'react-redux'
 import { Container } from './styles'
+
+import { N } from 'typings'
+import { IReducer } from 'reducers'
 
 interface IProps {
   colIndex: number
   rowIndex: number
 }
 
+interface IState {
+  value: N
+}
+
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container>
+  const state = useSelector<IReducer, IState>(({ grid }) => ({
+    value: grid ? grid[rowIndex][colIndex] : 0,
+  }))
+
+  return (
+    <Container data-cy={`block-${rowIndex}-${colIndex}`}>
+      {state.value === 0 ? '' : state.value}
+    </Container>
+  )
 }
 
 export default Block
--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,17 +1,23 @@
-import React, { FC, Children } from 'react'
-
-import { fillGrid, createFullGrid } from "utils";
-import { GRID } from "typings";
+import React, { FC, Children, useCallback, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { AnyAction, Dispatch } from 'redux'
+// import { fillGrid, createFullGrid } from 'utils'
+// import { GRID } from 'typings'
 
 import Block from './block'
 import { Container, Row } from './styles'
+import { createGrid } from 'reducers'
 
 const Grid: FC = () => {
-
-
-  const grid = createFullGrid();
-  console.log(grid);
-  
+  const dispatch = useDispatch<Dispatch<AnyAction>>()
+  const create = useCallback(() => dispatch(createGrid()), [dispatch])
+  /**
+   * 2번째 인자의 변수 추가시 해당 변수가 변할때 렌더링 발생
+   * 빈 배열이면 compoenetDidMount 와 유사하게 작동
+   */
+  useEffect(() => {
+    create()
+  }, [create])
 
   // 체크 포인트
   return (
