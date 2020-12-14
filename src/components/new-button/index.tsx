799b29bac4cebbe9ec0f3583c8a1754d0e0ad7f4
100milliongold <gadian88@gmail.com>
2020-12-14 23:04:19 +0900

    45. New Game

--- src/components/new-button/index.tsx
+++ src/components/new-button/index.tsx
@@ -1,8 +1,20 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { Action, Dispatch } from 'redux'
+
 import { Button } from 'components'
+import { createGrid } from 'reducers'
 
 const NewButton: FC = () => {
-  return <div></div>
+  const dispatch = useDispatch<Dispatch<Action>>()
+
+  const createNewGrid = useCallback(() => {
+    if (window.confirm('새로운 게임을 시작 하시겠습니까?')) {
+      dispatch(createGrid())
+    }
+  }, [dispatch])
+
+  return <Button onClick={createNewGrid}>New Game</Button>
 }
 
 export default NewButton
